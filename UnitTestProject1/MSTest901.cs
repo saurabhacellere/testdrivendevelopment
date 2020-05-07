3adf3ec1b66f14d39d4635f2a31cc40c52afcf45
saurabh.patil@acellere.com <saurabh.patil@acellere.com>
2020-05-07 18:48:11 +0530

    added 1000 files with 50k testcases for mstest

---
+++ UnitTestProject1/MSTest901.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class MSTest901
+    {
+		        [TestMethod]
+        public void TestMethod1()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod11()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod12()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod13()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod14()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod15()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod16()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod17()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod18()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod19()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod20()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod21()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod22()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod23()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod24()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod25()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod26()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod27()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod28()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod29()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod30()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod31()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod32()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod33()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod34()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod35()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod36()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod37()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod38()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod39()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod40()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod41()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod42()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod43()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod44()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod45()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod46()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod47()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod48()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+        [TestMethod]
+        public void TestMethod49()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod50()
+        {
+            Thread.Sleep(500);
+            Assert.AreEqual(1, 2);
+        }
+
+
+    }
+}
